b92fd15f995d5449e3fdb074cee42108455a4786
WojciechIskierkaEspeo <wojciech.iskierka@espeo.eu>
2022-02-21 11:44:22 +0100

    updated lint, added jsx

--- src/Pet.js
+++ src/Pet.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-export default function Pet({ name, animal, breed }) {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
-}
+const Pet = (props) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h2>{props.animal}</h2>
+      <h2>{props.breed}</h2>
+    </div>
+  );
+};
+
+export default Pet;
