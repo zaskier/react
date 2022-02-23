f04c166ce215bae35f8ff487bd3cf6046bec82a1
WojciechIskierkaEspeo <zaskier@gmail.com>
2022-02-23 09:22:21 +0100

    added reacr router with example detail route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
+import { StrictMode } from "react";
 import { render } from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SearchParams from "./SearchParam";
-
+import Details from "./details";
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-
-      <SearchParams />
+      <Router>
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route path="/">
+            <SearchParams />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
 
-render(<App />, document.getElementById("root"));
+render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
---
+++ src/Details.js
@@ -0,0 +1,4 @@
+const Details = () => {
+  return <h2> Lorem Ipsum is an random language or something</h2>;
+};
+export default Details;
