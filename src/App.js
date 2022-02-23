e81016b671c31ef9cfd7186496f2564c5539dd33
WojciechIskierkaEspeo <zaskier@gmail.com>
2022-02-23 12:01:40 +0100

    moved header of page to router with navigation url

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { StrictMode } from "react";
 import { render } from "react-dom";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import SearchParams from "./SearchParam";
 import Details from "./details";
 const App = () => {
   return (
     <div>
-      <h1>Adopt Me!</h1>
       <Router>
+        <header>
+          <Link to="/">
+            <h1>Adopt Me!</h1>
+          </Link>
+        </header>
         <Switch>
           <Route path="/details/:id">
             <Details />
